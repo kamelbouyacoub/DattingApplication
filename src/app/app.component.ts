c4a9ac409ef52091d4ad8445aeb93191fec6a481
BOUYACOUB Kamel <kamel_bouyacoub07@live.fr>
2021-03-23 22:01:26 +0100

    -first request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'DattingAppFront';
+export class AppComponent implements OnInit {
+  title = 'The dating app';
+  users: any;
+
+
+  /**
+   *
+   */
+  constructor(private http: HttpClient) {
+    
+
+  }
+  ngOnInit() {
+    this.getUsers();
+  }
+
+
+  getUsers(){
+    this.http.get(environment.userDating).subscribe((response) => {
+      this.users = response
+    }, error => {
+      console.log(error);
+    });
+  }
 }
